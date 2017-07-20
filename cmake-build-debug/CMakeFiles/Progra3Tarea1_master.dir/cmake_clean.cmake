ea6ae9d5fad682fe39071cf6049a9d4092779cf1
Oscarluiis <tinocoscar11@unitec.edu>
2017-07-20 07:26:07 -0600

    Ejercicios_Hechos

---
+++ cmake-build-debug/CMakeFiles/Progra3Tarea1_master.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Progra3Tarea1_master.dir/Evaluador.cpp.o"
+  "CMakeFiles/Progra3Tarea1_master.dir/main.cpp.o"
+  "Progra3Tarea1_master.pdb"
+  "Progra3Tarea1_master"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Progra3Tarea1_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
